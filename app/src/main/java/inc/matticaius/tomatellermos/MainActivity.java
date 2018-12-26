ef5b79e9268cdc85bb5e22a20f86dc7ed3b70f83
Matticaius <MatthiasAHolmberg@gmail.com>
2018-12-26 23:15:11 +0100

    pappa

--- app/src/main/java/inc/matticaius/tomatellermos/MainActivity.java
+++ app/src/main/java/inc/matticaius/tomatellermos/MainActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.view.animation.Animation;
 import android.view.animation.BounceInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,181 +23,106 @@ import android.widget.ImageView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements AccelerometerListener  {
+    private static final String COIN_SIDE = "coin side";
+    private static final int ANIM_DUR = 250;
+    private static final int TOMAT = R.drawable.tomat_small;
+    private static final int MOS = R.drawable.mos_small;
+    private static boolean waitforit = false;
 
-    public static final Random RANDOM = new Random();
-    private ImageView coin;
-    private Button btn;
+    private ImageView coinImage;
+
+    private Random r;
+    private int coinSide;
+    private int curSide = TOMAT;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        coin = findViewById(R.id.coin);
-        coin.setImageResource(R.drawable.tomat);
-        coin.setTag(R.drawable.tomat);
-        //btn = findViewById(R.id.btn);
-        //btn.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View view) {
-        //        //flipCoin();
-        //    }
-        //});
-        coin.setOnClickListener(new View.OnClickListener() {
+        r = new Random();
+        coinImage = findViewById(R.id.coin);
+
+        // Restore all values and images after rotate
+
+        if (savedInstanceState != null) {
+            coinImage.setImageResource(Integer.parseInt(savedInstanceState.getCharSequence(COIN_SIDE).toString()));
+        }
+
+        coinImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //flipAnimation();
-                flipCoin2();
+                flipCoin(coinImage);
             }
         });
     }
 
-
-    private void flipCoin2(){
-        flipAnimation();
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putCharSequence(COIN_SIDE, String.valueOf(curSide));
     }
 
-    private void flipAnimation(){
+    @Override
+    public void onPause() {
+        super.onPause();
 
-        final int rand = RANDOM.nextInt(5);
-        final ObjectAnimator oa1 = ObjectAnimator.ofFloat(coin, "scaleX", 1f, 0f);
-        final ObjectAnimator oa2 = ObjectAnimator.ofFloat(coin, "scaleX", 0f, 1f);
-        oa1.setInterpolator(new DecelerateInterpolator());
-        oa2.setInterpolator(new AccelerateDecelerateInterpolator());
-        oa1.addListener(new AnimatorListenerAdapter() {
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-                super.onAnimationRepeat(animation);
-                int draw = (int)coin.getTag();
-                switch (draw){
-                    case R.drawable.tomat:
-                        coin.setImageResource(R.drawable.mos);
-                        coin.setTag(R.drawable.mos);
-                        break;
-                    case R.drawable.mos:
-                        coin.setImageResource(R.drawable.tomat);
-                        coin.setTag(R.drawable.tomat);
-                        break;
-                    default:
-                        coin.setImageResource(R.drawable.error);
-                        coin.setTag(R.drawable.error);
-                        break;
-                }
-                oa2.start();
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                int draw = (int)coin.getTag();
-                switch (draw){
-                    case R.drawable.tomat:
-                        coin.setImageResource(R.drawable.mos);
-                        coin.setTag(R.drawable.mos);
-                        break;
-                    case R.drawable.mos:
-                        coin.setImageResource(R.drawable.tomat);
-                        coin.setTag(R.drawable.tomat);
-                        break;
-                    default:
-                        coin.setImageResource(R.drawable.error);
-                        coin.setTag(R.drawable.error);
-                        break;
-                }
-                oa2.start();
-            }
-        });
-        oa1.setRepeatCount(rand);
-        oa2.setDuration(500);
-        oa1.start();
     }
 
-    private void flipAnimation2(){
-        final ObjectAnimator oa1 = ObjectAnimator.ofFloat(coin, "scaleX", 1f, 0f);
-        final ObjectAnimator oa2 = ObjectAnimator.ofFloat(coin, "scaleX", 0f, 1f);
-        oa2.setDuration(500);
-        oa1.setDuration(500);
-        oa1.setInterpolator(new DecelerateInterpolator());
-        oa2.setInterpolator(new AccelerateDecelerateInterpolator());
-        oa1.addListener(new AnimatorListenerAdapter() {
+    private long animateCoin(boolean stayTheSame) {
 
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-                super.onAnimationRepeat(animation);
-                int draw = (int)coin.getTag();
-                switch (draw){
-                    case R.drawable.tomat:
-                        coin.setImageResource(R.drawable.mos);
-                        coin.setTag(R.drawable.mos);
-                        break;
-                    case R.drawable.mos:
-                        coin.setImageResource(R.drawable.tomat);
-                        coin.setTag(R.drawable.tomat);
-                        break;
-                    default:
-                        coin.setImageResource(R.drawable.error);
-                        coin.setTag(R.drawable.error);
-                        break;
-                }
-                oa2.start();
-            }
+        Rotate3dAnimation animation;
 
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                //int draw = (int)coin.getTag();
-                //switch (draw){
-                //    case R.drawable.tomat:
-                //        coin.setImageResource(R.drawable.mos);
-                //        coin.setTag(R.drawable.mos);
-                //        break;
-                //    case R.drawable.mos:
-                //        coin.setImageResource(R.drawable.tomat);
-                //        coin.setTag(R.drawable.tomat);
-                //        break;
-                //    default:
-                //        coin.setImageResource(R.drawable.error);
-                //        coin.setTag(R.drawable.error);
-                //        break;
-                //}
-                //oa2.start();
-            }
-        });
-        oa1.setRepeatCount(2);
-        //oa1.setRepeatMode(oa1.RESTART);
-        oa1.start();
+        if (curSide == TOMAT) {
+            animation = new Rotate3dAnimation(coinImage, TOMAT, MOS, 0, 180, 0, 0, 0, 0);
+        } else {
+            animation = new Rotate3dAnimation(coinImage, MOS, TOMAT, 0, 180, 0, 0, 0, 0);
+        }
+        if (stayTheSame) {
+            animation.setRepeatCount(3); // must be odd (5+1 = 6 flips so the side will stay the same)
+        } else {
+            animation.setRepeatCount(4); // must be even (6+1 = 7 flips so the side will not stay the same)
+        }
+
+        animation.setDuration(ANIM_DUR);
+        animation.setInterpolator(new LinearInterpolator());
+
+        coinImage.startAnimation(animation);
+        waitforit = true;
+
+        return animation.getDuration() * (animation.getRepeatCount() + 1);
     }
 
-    private void flipCoin() {
-        Animation fadeOut = new AlphaAnimation(1, 0);
-        fadeOut.setInterpolator(new AccelerateInterpolator());
-        fadeOut.setDuration(1000);
-        fadeOut.setFillAfter(true);
-        fadeOut.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
+    public void flipCoin(View v) {
+        if (!waitforit){
+            coinSide = r.nextInt(2);
 
+            if (coinSide == 0) {  // We have Tails
+                boolean stayTheSame = (curSide == MOS);
+                long timeOfAnimation = animateCoin(stayTheSame);
+                curSide = MOS;
+
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        waitforit = false;
+                    }
+                }, timeOfAnimation + 100);
+            } else {  // We have Heads
+                boolean stayTheSame = (curSide == TOMAT);
+                long timeOfAnimation = animateCoin(stayTheSame);
+                curSide = TOMAT;
+
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        waitforit = false;
+                    }
+                }, timeOfAnimation + 100);
             }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                coin.setImageResource(RANDOM.nextFloat() > 0.5f ? R.drawable.tomat : R.drawable.mos);
-
-                Animation fadeIn = new AlphaAnimation(0, 1);
-                fadeIn.setInterpolator(new DecelerateInterpolator());
-                fadeIn.setDuration(3000);
-                fadeIn.setFillAfter(true);
-
-                coin.startAnimation(fadeIn);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-
-        coin.startAnimation(fadeOut);
+        }
     }
 
     @Override
@@ -213,7 +140,7 @@ public class MainActivity extends AppCompatActivity implements AccelerometerList
 
     @Override
     public void onShake(float force) {
-        flipCoin2();
+        flipCoin(coinImage);
     }
 
     @Override
